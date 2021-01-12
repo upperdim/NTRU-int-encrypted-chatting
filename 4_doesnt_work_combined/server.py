93d42d533fea4d2b59051866a73e9d31c672f159
didyouforgettochangethis <>
2021-01-12 14:01:26 +0300

    fix var names on serverside sending section

--- 4_doesnt_work_combined/server.py
+++ 4_doesnt_work_combined/server.py
@@ -20,9 +20,9 @@ print("Connected to ", addr)
 while True:
     msg = input("Send message to client: ") # input str
     enc = encrypt_message(msg, q, f, g) # inputs str, returns int array
-    sendthis = pickle.dumps(d)
-    sendthis = bytes(f"{len(enc):<{HEADERSIZE}}", 'utf-8')+enc
-    con.send(enc) # gotta send int array
+    sendthis = pickle.dumps(enc)
+    sendthis = bytes(f"{len(sendthis):<{HEADERSIZE}}", 'utf-8')+sendthis
+    con.send(sendthis) # gotta send int array
 
     print("Waiting for reply...")
     recieved_msg = con.recv(1024)
